e37c652963ee921c93f553ffcca2c25c1c9b9117
ilyaYarosh88 <mygithubuser88@gmail.com>
2026-02-15 16:46:20 +0300

    Создал проект

---
+++ addressbook-web-tests/addressbook-web-tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("addressbook-web-tests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("addressbook-web-tests")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("53d194a9-f812-4e98-bce8-d397a95f6cbe")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
